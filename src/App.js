a7fa38eba45108e336e8b65517be1d76b7b84c10
iUtkarshVats <iUtkarshVats@users.noreply.github.com>
2020-04-29 00:52:26 +0530

    "basic todo list"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 // import './App.css';
 
-function App() {
+const App = () => {
+  const [newTodo, setNewTodo] = useState('');
+
+  const [todos, setTodos] = useState([]);
+
+  const onTodoChange = useCallback((event) => {
+    setNewTodo(event.target.value);
+  }, []);
+
+ const todoSubmitted = useCallback((event) => {
+    event.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        content: newTodo,
+        done: false
+      }
+    ]);
+    setNewTodo('');
+  }, [newTodo, todos]);
+  
+  useEffect(() => {
+    console.log('todos', todos);
+  }, [todos]);
+
   return (
-    <div className="App">
-      <h1>Hello!</h1>
+    <div>
+      <form onSubmit={todoSubmitted}>
+        <label htmlFor="newTodo">Enter a ToDo:</label>
+        <input
+          id = "newTodo"
+          name = "newTodo"
+          value = {newTodo}
+          onChange = {onTodoChange}
+        />
+        <button>+</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.content}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
+ 
 export default App;
